4d0498357d603927718cd79c94f2df6858bae2a3
itt06 <miyahara.itsuki4933@mail.kyutech.jp>
2026-05-01 17:25:40 +0900

    first commit

---
+++ game01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc,char** argv){
+    int a,b;
+
+    srand((unsigned) time(NULL));
+    int T[52];
+    
+    //トランプ用意
+    for(int i=0; i=<51; i++){
+        T[i] = i;
+    }
+
+    //トランプシャッフル
+    for(int j=0; j=<; j++){
+
+    }
+
+
+    int T[52];
+
+    scanf("%d"&a);
+    if(a )
+    return 0;
+}
