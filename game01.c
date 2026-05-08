66f6d9afc3ff0366ae5ef837128535428fa61226
itt06 <miyahara.itsuki4933@mail.kyutech.jp>
2026-05-08 16:03:18 +0900

    add show cards display and bug fixes

--- game01.c
+++ game01.c
@@ -6,36 +6,43 @@ int main(int argc,char** argv){
 
     //カード準備　cards[0][j]が黒、cards[1][j]が赤
     int cards[2][26];
-    for(int i=0; i<2; i++){
-        for(int j=0; j<25; j++){
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 26; j++){
             cards[i][j] = j;
         }
     }
 
 
     //フィッシャー・イェーツシャッフル
+    int tmp, row, col;
     srand((unsigned) time(NULL));
-    int ransu_i, ransu_j, tmp;
-    for(int i=0; i<50; i++){
-        if(i<26){
-            ransu_i = rand() % 2;
-            ransu_j = rand() % (26-i);
-            tmp = cards[ransu_i][ransu_j];
-            cards[ransu_i][ransu_j] = cards[1][25-i];
-            cards[1][25-i] = tmp;
+    for(int i = 0; i < 50; i++){
+        int ransu = rand() % (51 - i);
+        if(ransu > 25){         //通し番号をつけて二次元配列を一次元に対応させる
+            row = 1;
+            col = ransu - 26;
         }
         else{
-        ransu_j = rand() % (51-i);
-        tmp = cards[0][ransu_j];
-        cards[0][ransu_j] = cards[0][51-i];
-        cards[0][51-i] = tmp;
+            row = 0;
+            col = ransu;
         }
+        int row_last = 1, col_last = 25;       //cards[1][j]を全部入れ替えたらcards[0][j]に移行する
+        if(i > 25){
+            row_last = 0;
+            col_last = 51;
+        }
+        tmp = cards[row][col];
+        cards[row][col] = cards[row_last][col_last - i];
+        cards[row_last][col_last - i] = tmp;
     }
 
-    for(int i=0; i<2; i++){
-        for(int j=0; j<52; j++){
-            printf("%d\n", cards[i][j] );
+    //カードを表示する
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 13; j++){
+            printf("%d", cards[i % 2][i / 2 * 13 + j]);
+            printf(" ");
         }
+        printf("\n");
     }
 
     return 0;
